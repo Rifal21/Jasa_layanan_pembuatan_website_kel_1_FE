d0c4622df500a220850434b13ec39e44e5f96261
Rifal21 <giantjou77@gmail.com>
2023-06-06 17:04:24 +0700

    ubah layout code register

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -5,31 +5,31 @@
   <div class="col-md-4">
     <main class="form-signin w-100 m-auto text-center mb-5">       
 
-          <img class="mb-3" src="/img/Citta Nirmala.png" alt="" width="250" >
-          <h1 class="h3 mb-5 fw-normal">Please Login</h1>
-          <form action="/login" method="POST">
-            @csrf
-            <div class="form-floating">
-              <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
-              <label for="email">Email address</label>
-              @error('email')
-                <div class="invalid-feedback">
-                  {{ $message }}
-                </div>
-              @enderror
+      <img class="mb-3" src="/img/robot .png" alt="" width="250" >
+      <h1 class="h3 mb-5 fw-normal">IMR Login</h1>
+      <form action="/login" method="POST">
+        @csrf
+        <div class="form-floating">
+          <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
+          <label for="email">Email address</label>
+          @error('email')
+            <div class="invalid-feedback">
+              {{ $message }}
             </div>
-            <div class="form-floating">
-              <input type="password" name="password" class="form-control" id="Password" placeholder="Password" required>
-              <label for="Password">Password</label>
-            </div>
-            <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
-          </form>
-          <small class="d-block text-center mt-3">Not registered? <a href="/register" class="text-decoration-none">Register Now!</a></small>
-        </main> 
-      </div> 
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-  </body>
-</html>
+          @enderror
+        </div>
+        <div class="form-floating">
+          <input type="password" name="password" class="form-control" id="Password" placeholder="Password" required>
+          <label for="Password">Password</label>
+        </div>
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+      </form>
+      <small class="d-block text-center mt-3">Not registered? <a href="/register" class="text-decoration-none">Register Now!</a></small>
+    </main> 
+  </div> 
+</div>
+@endsection
+
+
 
 
