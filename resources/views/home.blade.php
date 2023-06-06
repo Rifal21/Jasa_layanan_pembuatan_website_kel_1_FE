57982098646cc7ede66753d0a4f193d71e58f42a
Rifal21 <giantjou77@gmail.com>
2023-06-06 14:12:02 +0700

    menambahkan elemen di fitur home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,13 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hello World !!</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1 class="text-center fs-bold">Hello, world!</h1>
-    <h3 class="text-center fs-bold">Ini Website IMR</h3>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-  </body>
-</html>
+@extends('layouts.main')
+@section('container')
+    <div class="row">
+      <div class="col">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus numquam, fugit accusantium quia aut temporibus aliquid! Sint, natus porro iste, itaque iusto tempora expedita ut modi vel corporis impedit culpa.</p>
+        <a href="#" class="btn btn-warning text-dark fw-bold mb-3">Pesan Sekarang</a>
+        <a href="#" class="btn btn-light text-dark fw-bold mb-3">Layanan Kami</a>
+      </div>
+      <div class="col">
+        <img src="/img/robot.png" class="img-fluid" alt="">
+      </div>
+    </div>
+@endsection
