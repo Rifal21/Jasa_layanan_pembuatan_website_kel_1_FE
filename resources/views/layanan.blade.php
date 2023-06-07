04fa9de6e03f6614c0cf231da14fb37819797d4d
Rifal21 <giantjou77@gmail.com>
2023-06-08 06:48:15 +0700

    add page detail layanan

--- resources/views/layanan.blade.php
+++ resources/views/layanan.blade.php
@@ -7,7 +7,7 @@
           <div class="row justify-content-center">
             <h3 class="mb-3 pt-5" style="color: #3EB772"><i class="bi bi-arrow-right-square"></i> Pilih sesuai dengan kebutuhan</h3>
             <div class="col-md-4 mb-5">
-              <a href="#" class="text-decoration-none" style="color: #3EB772">
+              <a href="#pilih" class="text-decoration-none" style="color: #3EB772">
                 <div class="card rounded border-success bg-transparent">
                   <img src="/img/ly5.png" class="card-img-top mx-auto" alt="..." style="width: 181px">
                   <div class="card-body text-center">
@@ -18,7 +18,7 @@
               </a>
             </div>
             <div class="col-md-4 mb-5">
-              <a href="#" class="text-decoration-none" style="color: #3EB772">
+              <a href="#" class="text-decoration-none" style="color: #3EB772" onclick="return alert('sorry cooming soon dude 😎')">
                 <div class="card rounded border-success bg-transparent">
                   <img src="/img/ly4.png" class="card-img-top mx-auto" alt="..." style="width: 181px">
                   <div class="card-body text-center">
@@ -29,7 +29,7 @@
               </a>
             </div>
             <div class="col-md-4 mb-5">
-              <a href="" class="text-decoration-none" style="color: #3EB772">
+              <a href="#" class="text-decoration-none" style="color: #3EB772" onclick="return alert('sorry cooming soon dude 😎')">
                 <div class="card rounded border-success bg-transparent">
                   <img src="/img/ly6.png" class="card-img-top mx-auto" alt="..." style="width: 181px">
                   <div class="card-body text-center">
@@ -41,9 +41,9 @@
             </div>
           </div>
           <div class="row justify-content-center">
-            <h3 class="mb-3 pt-5" style="color: #3EB772"><i class="bi bi-arrow-right-square"></i> Pilih layanan website yang tersedia</h3>
+            <h3 id="pilih" class="mb-3 pt-5" style="color: #3EB772"><i class="bi bi-arrow-right-square"></i> Pilih layanan website yang tersedia</h3>
             <div class="col-md-4 mb-5">
-              <a href="#" class="text-decoration-none" style="color: #3EB772">
+              <a href="/layanandet" class="text-decoration-none" style="color: #3EB772">
                 <div class="card rounded border-success bg-transparent">
                   <img src="/img/ly3.png" class="card-img-top mx-auto" alt="..." style="width: 181px">
                   <div class="card-body text-center">
@@ -55,7 +55,7 @@
               </a>
             </div>
             <div class="col-md-4 mb-5">
-              <a href="#" class="text-decoration-none" style="color: #3EB772">
+              <a href="/layanandet" class="text-decoration-none" style="color: #3EB772">
                 <div class="card rounded border-success bg-transparent">
                   <img src="/img/ly2.png" class="card-img-top mx-auto" alt="..." style="width: 181px">
                   <div class="card-body text-center">
@@ -67,7 +67,7 @@
               </a>
             </div>
             <div class="col-md-4 mb-5">
-              <a href="#" class="text-decoration-none" style="color: #3EB772">
+              <a href="/layanandet" class="text-decoration-none" style="color: #3EB772">
                 <div class="card rounded border-success bg-transparent">
                   <img src="/img/ly1.png" class="card-img-top mx-auto" alt="..." style="width: 181px">
                   <div class="card-body text-center">
