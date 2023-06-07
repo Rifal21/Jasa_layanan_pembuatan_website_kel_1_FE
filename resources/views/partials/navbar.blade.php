a50c4e5d5423b334b4aa2363c6198f2ec0502e12
Rifal21 <giantjou77@gmail.com>
2023-06-07 20:51:05 +0700

    added element layanan

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 
       <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #3EB772">
         <div class="container">
-          <a class="navbar-brand" href="#">IMR</a>
+          <a class="navbar-brand" href="/">IMR</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -9,8 +9,8 @@
             <div class="navbar-nav ms-auto px-4">
               <a class="nav-link " aria-current="page" href="/">Home</a>
               <a class="nav-link" href="/layanan">Layanan</a>
-              <a class="nav-link" href="#">Tentang</a>
-              <a class="nav-link" href="#">Portofolio</a>
+              <a class="nav-link" href="/tentang">Tentang</a>
+              <a class="nav-link" href="/portofolio">Portofolio</a>
             </div>
             <a class="nav-link btn btn-warning text-white rounded" href="/login">Masuk</a>
           </div>
