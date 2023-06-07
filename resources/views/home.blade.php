592a6af50bb37a98405b371e8676e641ab371733
Rifal21 <giantjou77@gmail.com>
2023-06-07 14:06:28 +0700

    fix position hero section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.main')
 @section('container')
-    <div class="row">
-      <div class="col">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus numquam, fugit accusantium quia aut temporibus aliquid! Sint, natus porro iste, itaque iusto tempora expedita ut modi vel corporis impedit culpa.</p>
+    <div class="row justify-content-center d-flex align-items-center">
+      <div class="col-md-6 ">
+        <h1>Welcome to IMR Website</h1>
+        <p class="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus numquam, fugit accusantium quia aut temporibus aliquid! Sint, natus porro iste, itaque iusto tempora expedita ut modi vel corporis impedit culpa.</p>
         <a href="#" class="btn btn-warning text-dark fw-bold mb-3">Pesan Sekarang</a>
         <a href="#" class="btn btn-light text-dark fw-bold mb-3">Layanan Kami</a>
       </div>
